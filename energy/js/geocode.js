7bcdcf1fc984e68af62a0d7eb12303a72c9ddc16
Matt Triff <matt.triff@gmail.com>
2013-04-21 03:49:12 -0400

    Geocode functions now available

---
+++ energy/js/geocode.js
@@ -0,0 +1,38 @@
+var geocoder;
+
+function lookup(address)
+{
+	geocoder=new google.maps.Geocoder();
+	geocoder.geocode({'address':address}, function(results, status){
+		if(status==google.maps.GeocoderStatus.OK)
+		{
+			list="";
+			for(var i=0; i<5; i++)
+			{
+				if(results[i])
+				{
+					console.log(results[i].formatted_address);
+				}
+			}
+		}
+	})
+}
+
+function reverse(lat, lng)
+{
+	geocoder=new google.maps.Geocoder();
+	var latlng=new google.maps.LatLng(lat, lng);
+	geocoder.geocode({'location':latlng}, function(results, status){
+		if(status==google.maps.GeocoderStatus.OK)
+		{
+			list="";
+			for(var i=0; i<5; i++)
+			{
+				if(results[i])
+				{
+					console.log(results[i].formatted_address);
+				}
+			}			
+		}
+	})	
+}
