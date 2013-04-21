ae95094d026391961e4bdefef668932e8af6e412
Gabriel O'FLaherty-Chan <hi@gabrieloc.com>
2013-04-21 04:53:08 -0400

    title

--- energy/javascript/begin2.js
+++ energy/javascript/begin2.js
@@ -3,10 +3,7 @@ var socket = io.connect();
 socket.on("request", function(data){
 	console.log("cb: request:"+data);
 	data=JSON.stringify(data);
-	log.innerHTML=log.innerHTML.concat(data);
-
-	var someid = document.getElementById("textbox1");
-	someid.innerHTML = data.field;
+	console.log(data);
 
 });
 
