efd0fdcbf041432647b4fafc235e78483e0bf7a9
Matt Triff <matt.triff@gmail.com>
2013-04-21 13:02:58 -0400

    Distance func

--- energy/javascript/begin2.js
+++ energy/javascript/begin2.js
@@ -4,6 +4,22 @@ socket.on("request", function(data){
 	console.log("cb: request:"+data);
 	// data=JSON.stringify(data);
 
+	var distanceFromHome=-1;
+
+	if ("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+				var R = 6371; // km
+				var dLat = (data['latitude']-position.coords.latitude).toRad();
+				var dLon = (data['longitude']-position.coords.longitude).toRad();
+				var lat1 = lat1.toRad();
+				var lat2 = lat2.toRad();
+
+				var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+				        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+				var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+				distanceFromHome = R * c;
+		});
+
 	$('#mapInfo').filter(function(){
 		var panel = $(this);
 		console.log(reverse);
