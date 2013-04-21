15d85cf0de707ff0fd2c3882fa946de52bf73862
Michael Law <mlaw135@gmail.com>
2013-04-21 02:38:58 -0400

    finalize

--- energy/js/safehouse.js
+++ energy/js/safehouse.js
@@ -97,6 +97,7 @@ $(document).ready(function() {
 
 		map.on('dragend', function(e) {
 			console.log("marker set to",marker.getLatLng());
+			finalize();
 		});
 
 
@@ -178,76 +179,6 @@ $(document).ready(function() {
 			}
 		}
 
-// <<<<<<< HEAD
-// =======
-		//Given a JSON string from the server, determine the number of solar panels and wind turbines required 
-		function findNumProducts(jsonStr){
-			var obj=eval("(" + jsonStr + ')');
-			solarMonths=obj["solar"]["value"];
-			windMonths=obj["wind"]["value"];
-
-			//minMonth is the lowest value of monthly values
-			var minMonth=0;
-			var minValue=parseFloat(solarMonths[0])+parseFloat(windMonths[0]);
-			for(var i=1; i<12; i++)
-			{
-				var currValue=parseFloat(solarMonths[0])+parseFloat(windMonths[0]);
-				if(currValue<minValue)
-				{
-					minMonth=i;
-					minValue=currValue;
-				}
-			}
-
-			minSolar=parseFloat(solarMonths[minMonth]);
-			minWind=parseFloat(windMonths[minMonth]);
-			var numSolarPanels=0;
-			var numWindTurbines=0;
-			if(minSolar==0)
-			{
-				//must calculate 335 using only wind
-				kwhPerWindTurbine=minWind*20;
-				powerGenerated=kwhPerWindTurbine*numWindTurbines;
-				while(powerGenerated<335)
-				{
-					numWindTurbines++;
-				}
-			}
-			else if(minWind==0)
-			{
-				//must calculate 335kWh using only solar
-				kwhPerSolarPanel=minSolar*30*1.66*0.15;
-				powerGenerated=kwhPerSolarPanel*numSolarPanels;
-				while(powerGenerated<355)
-				{
-					numSolarPanels++;
-				}
-			}
-			else
-			{
-				//Calculate, aiming for 167kWh from solar, 167kWh from wind
-				kwhPerWindTurbine=minWind*20;
-				powerGenerated=kwhPerWindTurbine*numWindTurbines;
-				while(powerGenerated<167)
-				{
-					numWindTurbines++;
-				}
-
-				kwhPerSolarPanel=minSolar*30*1.66*0.15;
-				powerGenerated=kwhPerSolarPanel*numSolarPanels;
-				while(powerGenerated<355)
-				{
-					numSolarPanels++;
-				}
-			}
-
-			var products={"solarPanels":numSolarPanels,"windTurbines":numWindTurbines};
-
-			return products;
-		}
-
-
-// >>>>>>> a2715e3562e4a785adf9f7ac305bf78cdf35176c
 		function resetHighlight(e) {
 		    geojson.resetStyle(e.target);
 		    info.update();
@@ -262,11 +193,9 @@ $(document).ready(function() {
 		    });
 		}
 
-		var submitButton = document.getElementById('submitButton');
-		submitButton.addEventListener('click', finalize, false);
-
 		function finalize(e) {
 
+			console.log("FINALIZE CALLED");
 			var center = map.getCenter();
 			console.log('finalizing coordinates at ', center);
 			map.setView(center, 5);
